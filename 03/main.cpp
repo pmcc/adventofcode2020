d86737196986a703e4a8cc20abca2d20fe9264e9
Pierre-Marie Charavel <pmcharavel@fastmail.com>
2020-12-03 15:07:27 -0500

    Day 3
    Part 2

--- 03/main.cpp
+++ 03/main.cpp
@@ -17,6 +17,8 @@ int main(int argc, char** argv)
         return 0;
     }
 
+    // Part 1
+
     constexpr int x_step = 3;
     constexpr int y_step = 1;
 
@@ -45,6 +47,33 @@ int main(int argc, char** argv)
     }
 
     std::cout << happy_little_trees << std::endl;
+
+    // Part 2
+
+    auto get_trees_on_slope = [&map, width, height](int x_step, int y_step)
+    {
+        int happy_little_trees = 0;
+
+        for (int x = 0, y = 0; y < height; y += y_step)
+        {
+            if (map[y][x % width] == '#')
+            {
+                ++happy_little_trees;
+            }
+
+            x += x_step;
+        }
+
+        return happy_little_trees;
+    };
+
+    int result = get_trees_on_slope(1, 1)
+        * get_trees_on_slope(3, 1)
+        * get_trees_on_slope(5, 1)
+        * get_trees_on_slope(7, 1)
+        * get_trees_on_slope(1, 2);
+
+    std::cout << result << std::endl;
 
     return 0;
 }
